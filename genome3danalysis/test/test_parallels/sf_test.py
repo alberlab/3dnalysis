2c50020e9220cc0647c7c7e7b5ccd3fc5d255c6b
Francesco Musella <fmusella@g.ucla.edu>
2023-07-19 18:03:49 -0700

    changed for H1 with appropriate parameters, need to include gap, speckle, nucleoli, AB files

--- genome3danalysis/test/test_parallels/sf_test.py
+++ genome3danalysis/test/test_parallels/sf_test.py
@@ -5,32 +5,36 @@ import os
 # Set working directory as file directory
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-hss_name = 'igm-model_mcrb_2.5MB.hss'
-# append absolute path to hss_name
+hss_name = 'igm-model.damid_0.2000.inter_sigma_0.0040.intra_sigma_0.0080.sprite_5.0.hss'
 hss_name = os.path.join(os.path.dirname(os.path.abspath(__file__)), hss_name)
 
 sf_name = hss_name.replace('.hss', '.sf')
 sf = SfFile(sf_name, 'w')
 
 config = {'hss_name': hss_name,
-          'gap_name': '',  # ADD GAP FILE!
-          'features': {'radial': {'shape': 'ellipsoid',
-                                  'radius': [3050, 2350, 2350],
+          'gap_name': 'ADD GAP FILE!',
+          'features': {'radial': {'shape': 'sphere',
+                                  'radius': 5000.,
                                   'contact_threshold': 0.5},
-                       'lamina': {'shape': 'ellipsoid',
-                                  'radius': [3050, 2350, 2350],
-                                  'contact_threshold': 770},
-                       'lamina_tsa': {'shape': 'ellipsoid',
-                                      'radius': [3050, 2350, 2350],
-                                      'exterior_threshold': 0.8,
-                                      'tsa_exponent': 0.007},
-                       'speckle': {'filename': '/u/home/f/fmusella/genome3danalysis/genome3danalysis/test/test_parallels/bodies.pkl'},
-                       'transAB': {'filename': '/u/home/f/fmusella/genome3danalysis/genome3danalysis/test/test_parallels/mcrb_yamada_AB_2.5Mb.bed',
-                                   'dist_cutoff': 200},
-                       'icp': {'dist_cutoff': 200},
-                       'rg': {'window': 7}
+                       'lamina': {'shape': 'sphere',
+                                  'radius': 5000.,
+                                  'contact_threshold': 0.2 * 5000.},
+                       'lamina_tsa': {'shape': 'sphere',
+                                      'radius': 5000.,
+                                      'exterior_threshold': 0.85,
+                                      'tsa_exponent': 0.004},
+                       'speckle': {'filename': 'ADD SPECKLE FILE!'},
+                       'speckle_tsa': {'filename': 'ADD SPECKLE FILE!',
+                                       'tsa_exponent': 0.004},
+                       'nucleoli': {'filename': 'ADD NUCLEOLI FILE!'},
+                       'nucleoli_tsa': {'filename': 'ADD NUCLEOLI FILE!',
+                                        'tsa_exponent': 0.004},
+                       'transAB': {'filename': 'ADD AB FILE!',
+                                   'dist_cutoff': 500},
+                       'icp': {'dist_cutoff': 500},
+                       'rg': {'window': 5}
                        },
-          'parallel': {'controller': 'serial'}
+          'parallel': {'controller': 'ipyparallel'}
           }
 
 sf.run(config)
