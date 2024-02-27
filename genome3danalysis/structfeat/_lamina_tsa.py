1b7189491d947378792448b5a0a81f9ed11a1059
Francesco Musella <fmusella@g.ucla.edu>
2024-02-26 18:53:00 -0800

    comments added in laminaTSA

--- genome3danalysis/structfeat/_lamina_tsa.py
+++ genome3danalysis/structfeat/_lamina_tsa.py
@@ -77,13 +77,12 @@ def run(struct_id: int, hss: HssFile, params: dict) -> np.ndarray:
         # Compute √((X/a)² + (Y/b)² + (Z/c)²)
         rad_dist_scaled = np.linalg.norm(coord_scaled - center_scaled, axis=1)
         # Find the exterior beads and their coordinates
-        lamina_beads = np.where(rad_dist_scaled > np.quantile(rad_dist_scaled,
-                                                              ext_thr))[0]
+        lamina_beads = np.where(rad_dist_scaled > np.quantile(rad_dist_scaled, ext_thr))[0]
         lamina_coord = coord[lamina_beads, :]
         # Compute distance from every bead of the model to every bead of the lamina
-        dist_to_lamina = cdist(coord, lamina_coord, 'euclidean')  # (nbead, nbead_lamina)
+        dist_to_lamina = cdist(coord, lamina_coord, 'euclidean')  # (nbead, nbead_lamina)  TODO: CHECK: ASLI TOOK THE MINIMUM OF THESE DISTANCES?
         # The TSA signal is then taken as a negative exponential of the distances previously computed
-        lamina_tsa = np.sum(np.exp(- tsa_alpha * dist_to_lamina), axis=1)  # (nbead,)  CHECK SUM, MEAN OR MIN
+        lamina_tsa = np.sum(np.exp(- tsa_alpha * dist_to_lamina), axis=1)  # (nbead,)  TODO: CHECK SUM, MEAN OR MIN
         return lamina_tsa
     
     # for experimental
